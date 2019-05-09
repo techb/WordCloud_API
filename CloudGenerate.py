fed0ed810047bb67eec855c5b024a5cec06bd6e2
kbcarte <kbcarte@gmail.com>
2019-05-08 20:01:44 -0400

    inital commit

---
+++ CloudGenerate.py
@@ -0,0 +1,15 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import matplotlib.cm
+import pandas as pd
+
+
+class CloudGenerate():
+	def __init(self)__:
+		pass
+
+	def make_cloud(self, terms, cm='twilight', bcolor='black', alpha=False, w=800, h=800):
+		pass
+
+	def color_list(self):
+		pass
