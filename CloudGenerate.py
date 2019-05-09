96fec1fc863efb0cab66576b3f2ad39c9fabccf6
kbcarte <kbcarte@gmail.com>
2019-05-08 21:45:14 -0400

    implemented REST API, added functionality to CloudGenerate.py

--- CloudGenerate.py
+++ CloudGenerate.py
@@ -1,15 +1,32 @@
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import matplotlib.cm
+import numpy as np
 import pandas as pd
+from io import BytesIO
+from PIL import Image
 
 
 class CloudGenerate():
-	def __init(self)__:
+	def __init__(self):
 		pass
 
-	def make_cloud(self, terms, cm='twilight', bcolor='black', alpha=False, w=800, h=800):
-		pass
+	def make_cloud(self, terms, cm='twilight', w=800, h=800):
+		try:
+			# colormap ref: https://matplotlib.org/gallery/color/colormap_reference.html
+			wc = WordCloud(width=w, height=h, colormap=cm)
+			wc.generate(terms)
+			# img = wc.to_image()
+			img = wc.to_array()
+			img = Image.fromarray(img)
+
+			img_io = BytesIO()
+			img.save(img_io, 'JPEG', quality=70)
+			img_io.seek(0)
+			return img_io
+
+		except Exception as e:
+			return 'error', str(e)
 
 	def color_list(self):
-		pass
+		return matplotlib.cm.cmap_d.keys()
---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, send_file
+from flask_restful import Resource, Api, reqparse
+import json, os
+
+# import and create our cloud generator
+import CloudGenerate as CG
+cg = CG.CloudGenerate()
+
+app = Flask(__name__)
+api = Api(app)
+parser = reqparse.RequestParser()
+
+class Colors(Resource):
+	def __init__(self):
+		pass
+
+	def get(self):
+		colors = cg.color_list()
+		color_list = {'colors': []}
+		for c in colors:
+			color_list['colors'].append(c)
+
+		return color_list
+
+
+class MuhCloud(Resource):
+	def __init__(self):
+		pass
+
+	def post(self):
+		parser.add_argument('terms', location='json')
+		parser.add_argument('color', location='json')
+		args = parser.parse_args()
+
+		img = cg.make_cloud(args['terms'], cm=args['color'])
+
+		# checking type for errors. in the try block, return a tuple
+		# if an error happend: ('error', <The Error Message>)
+		if type(img) is tuple:
+			return {img[0]: img[1]}
+		else:
+			return send_file(img, mimetype='image/jpeg')
+
+# Add the endpoints
+api.add_resource(Colors, '/colors')
+api.add_resource(MuhCloud, '/cloud')
+
+# start in debug mode so we have live reloading on code changes
+# port 10798 == ord('k') + ord('b')
+app.run(port=10798, debug=True)
